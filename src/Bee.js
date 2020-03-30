79018cb41a0296a91c9d4fe896b50f8b252251cf
Michael Justice <m_a_justice@yahoo.com>
2020-03-30 13:31:40 -0700

    completed all bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+  // call the grub superclass
+  Grub.call(this);
+  // age property = 5
+  this.age = 5;
+  // color property = yellow
+  this.color = 'yellow';
+  // job property = keep on growing
+  this.job = 'keep on growing';
+  // food property inherited from grub
+  // eat method inherited from grub
 };
-
+// set the prototype
+Bee.prototype = Object.create(Grub.prototype);
+// set the constructor
+Bee.prototype.constructor = Bee;
